bddf27b27db56cfbd2870d6299e82ac315271597
Pham Huu Tri <trib2014624@student.ctu.edu.vn>
2024-02-21 10:30:00 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/route/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
-const contactsRouter = require("./app/route/contact.route");
+
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +14,16 @@ app.get("/", (req, res)=>{
 });
 
 app.use("/api/contacts", contactsRouter);
+
+//404
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    });
+});
 
 module.exports=app;
