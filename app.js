37a8be86a7979d05de5b645b4356b4b456034f29
Pham Huu Tri <trib2014624@student.ctu.edu.vn>
2024-02-21 10:01:47 +0700

    Cai dat cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/route/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res)=>{
     res.json({message : "Welcone to contacts book aplication!"});
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports=app;
